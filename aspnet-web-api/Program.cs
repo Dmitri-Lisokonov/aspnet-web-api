0fcd2458aa762425d9eb520d94f77389305e4b88
Dmitri Lisokonov <dmitri.lisokonov@gmail.com>
2021-12-20 14:27:55 +0100

    changed to https

--- aspnet-web-api/Program.cs
+++ aspnet-web-api/Program.cs
@@ -19,7 +19,7 @@ namespace aspnet_web_api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://0.0.0.0:5000");
+                    webBuilder.UseUrls("https://0.0.0.0:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
